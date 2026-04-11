0214de8a4c53c39013e8b4f631154eff0636046a
Gustavo Savi <gutosavi@hotmail.com>
2026-04-11 19:07:24 -0300

    styles: poster movie layout

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,20 +4,25 @@ import './MovieCard.css';
 const MovieCard = ({ movies }) => {
   return (
     <>
-      <ul className="movie-card">
+      <ul className="movie-container">
         {movies &&
           movies.map((movie) => (
             <li key={movie.id}>
-              <div className="movie-poster">
-                <img
-                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                />
-              </div>
-              <div className="movie-info">
-                <p className="movie-title">{movie.title}</p>
-                <p>{movie.vote_average}</p>
-                <p className="description">{movie.overview}</p>
-              </div>
+              <section className="movie-card">
+                <div className="movie-poster">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  />
+                </div>
+                <div className="movie-info">
+                  <p className="movie-title">{movie.title}</p>
+                  <p>{movie.vote_average}</p>
+                  <div className="hidden-content">
+                    <p className="description">{movie.overview}</p>
+                    <button className="btn-veja-mais">Veja mais</button>
+                  </div>
+                </div>
+              </section>
             </li>
           ))}
       </ul>
