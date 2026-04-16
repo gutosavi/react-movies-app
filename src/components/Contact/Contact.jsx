9ddfe58a3503c69e5bc6fa9fd08258f840ccc0ee
Gustavo Savi <gutosavi@hotmail.com>
2026-04-16 20:17:28 -0300

    style: contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Vamos conversar</h1>
+          <p>Fale-me sobre seu projeto</p>
+          <p>Vamos criar alguma coisa juntos.</p>
+        </div>
+        <div>
+          <p>Me mande um e-mail</p>
+          <span>gutosavi@hotmail.com</span>
+        </div>
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder="Nome completo" />
+          <input type="mail" placeholder="Endereço de e-mail" />
+          <textarea name="text-area" id="text-area" rows="10" cols="30">
+            Me fale um pouco sobre seu projeto:
+          </textarea>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
