12662d6331fe1597eb68c62c7de9f8d9e25d2639
Gustavo Savi <gutosavi@hotmail.com>
2026-05-08 15:55:39 -0300

    chore: implement typescrit configuration

---
+++ src/services/apiPost.tsx
@@ -0,0 +1,23 @@
+import { UserResponse } from "../types";
+
+export const fetchPostApi = async (data: UserResponse): Promise<UserResponse | null> => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('Erro na requisição');
+    }
+    const result: UserResponse = await response.json();
+    console.log('Dados enviados com sucesso', result);
+    return result;
+  } catch (error) {
+    console.error('Erro', error);
+    throw error;
+  }
+};
