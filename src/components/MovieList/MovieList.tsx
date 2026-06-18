b74517e9c5b1707b8a893651db7a2e894f94a687
Gustavo Savi <gutosavi@hotmail.com>
2026-06-17 21:22:15 -0300

    refactor: implementa botões de paginação

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -3,8 +3,9 @@ import styles from './MovieList.module.css';
 import MovieCard from '../MovieCard/MovieCard';
 import { OrbitProgress } from 'react-loading-indicators';
 import { fetchMovies } from '../../services/apiGet';
-import useDebounce from '../../hooks/useDebounce';
 import { Movie, MovieProps } from '../../types';
+import useDebounce from '../../hooks/useDebounce';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 
 const MovieList = ({ inputValue, movie, setMovie }: MovieProps) => {
   const [isLoading, setIsLoading] = React.useState(false);
@@ -21,34 +22,36 @@ const MovieList = ({ inputValue, movie, setMovie }: MovieProps) => {
     setFilterList(resultado);
   }, [debouncedSearchTerm, movie]);
 
+  // React.useEffect(() => {
+  //   if (isLoading) return;
+
+  //   function infiniteScroll() {
+  //     const scroll = window.scrollY;
+  //     const height = document.documentElement.scrollHeight - window.innerHeight;
+  //     if (scroll > height * 0.75) {
+  //       setPage((prev) => prev + 1);
+  //     }
+  //   }
+  //   window.addEventListener('scroll', infiniteScroll);
+  //   return () => {
+  //     window.removeEventListener('scroll', infiniteScroll);
+  //   };
+  // }, [isLoading]);
+
+  // infiniteScroll
+  // setMovie((prev) => {
+  //   const ids = new Set(prev.map((m) => m.id));
+  //   const newMovies = data.results.filter((m) => !ids.has(m.id));
+  //   return [...prev, ...newMovies];
+  // });
+
   React.useEffect(() => {
-    if (isLoading) return;
-
-    function infiniteScroll() {
-      const scroll = window.scrollY;
-      const height = document.documentElement.scrollHeight - window.innerHeight;
-      if (scroll > height * 0.75) {
-        setPage((prev) => prev + 1);
-      }
-    }
-    window.addEventListener('scroll', infiniteScroll);
-    return () => {
-      window.removeEventListener('scroll', infiniteScroll);
-    };
-  }, [isLoading]);
-
-  React.useEffect(() => {
-    if (isLoading) return;
-
-    setIsLoading(true);
     const getMovie = async () => {
+      setIsLoading(true);
+
       try {
         const data = await fetchMovies(page);
-        setMovie((prev) => {
-          const ids = new Set(prev.map((m) => m.id));
-          const newMovies = data.results.filter((m) => !ids.has(m.id));
-          return [...prev, ...newMovies];
-        });
+        setMovie(data.results);
       } catch (error) {
         console.error('Erro ao carregar os filmes', error);
       } finally {
@@ -68,8 +71,20 @@ const MovieList = ({ inputValue, movie, setMovie }: MovieProps) => {
         <section className={styles.movieList}>
           <MovieCard movie={filterList} />
           <div className={styles.buttonContainer}>
-            <button className={styles.movieButton}>Anterior</button>
-            <button className={styles.movieButton}>Próximo</button>
+            <button
+              disabled={page === 1 ? true : false}
+              onClick={() => setPage((prev) => prev - 1)}
+              className={styles.movieButton}
+            >
+              <ArrowLeft />
+            </button>
+            <button
+              disabled={page === 57473 ? true : false}
+              onClick={() => setPage((prev) => prev + 1)}
+              className={styles.movieButton}
+            >
+              <ArrowRight />
+            </button>
           </div>
         </section>
       )}
